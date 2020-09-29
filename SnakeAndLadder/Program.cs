92e725f2d79e9d522d686106db16ef72f5b2af08
Mathura <mathuradas333@gmail.com>
2020-09-29 15:35:48 +0530

    [Mathura]Add . Play Option in UC3

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -15,12 +15,36 @@ namespace SnakeAndLadder
             Console.WriteLine("Your Position : "+ position);
             int diceNo = SnakeAndLadder.Program.RollDice();
             Console.WriteLine("Dice rolled : "+ diceNo);
+            int option = SnakeAndLadder.Program.CheckOption();
+            switch(option)
+            {
+                case 0:
+                    Console.WriteLine("No Play");
+                    Console.WriteLine("New Position : " + position);
+                    break;
+                case 1:
+                    Console.WriteLine("Ladder");
+                    position += diceNo;
+                    Console.WriteLine("New Position : "+ position);
+                    break;
+                case 2:
+                    Console.WriteLine("Snake");
+                    position -= diceNo;
+                    Console.WriteLine("New Postion : "+ position);
+                    break;
+            }
         }
         static int RollDice()
         {
             Random random = new Random();
             int dice = random.Next(1, 7);
             return dice;
+        }
+        static int CheckOption()
+        {
+            Random random = new Random();
+            int option = random.Next(0, 3);
+            return option;
         }
     }
 }
