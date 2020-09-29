531f2f3dc0fa7a4adf0928b89d34e2817fe38817
Mathura <mathuradas333@gmail.com>
2020-09-29 15:19:55 +0530

    [Mathura]Add . Start posiion in UC1

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,6 +7,11 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Sanke and Ladder game");
+            SnakeAndLadder.Program.PlayGame();        
+        }
+        static void PlayGame()
+        {
+            int position = 0;
         }
     }
 }
